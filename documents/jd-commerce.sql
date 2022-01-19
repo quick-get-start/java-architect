1e7c32974b070666a6aad4a9bb560a6b877b8da4
张喜硕 <zhangxishuo1998@outlook.com>
2022-01-19 12:33:47 +0800

    assets

--- documents/jd-commerce.sql
+++ documents/jd-commerce.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : .
- Source Server Type    : MariaDB
- Source Server Version : 100313
- Source Host           : localhost:3306
- Source Schema         : foodie-shop-dev
-
- Target Server Type    : MariaDB
- Target Server Version : 100313
- File Encoding         : 65001
-
- Date: 01/09/2019 16:44:28
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
@@ -39,10 +23,10 @@ CREATE TABLE `carousel` (
 -- Records of carousel
 -- ----------------------------
 BEGIN;
-INSERT INTO `carousel` VALUES ('c-10011', 'http://122.152.205.72:88/group1/M00/00/05/CpoxxF0ZmG-ALsPRAAEX2Gk9FUg848.png', '#f44661', 'nut-1004', '', 1, 1, 1, '2019-08-27 20:33:06', '2019-08-24 20:33:09');
-INSERT INTO `carousel` VALUES ('c-10013', 'http://122.152.205.72:88/group1/M00/00/05/CpoxxF0ZmHiAWwR7AAFdqZHw8jU876.png', '#000240', '', '51', 2, 2, 1, '2019-08-25 20:33:06', '2019-08-25 20:33:09');
-INSERT INTO `carousel` VALUES ('c-10015', 'http://122.152.205.72:88/group1/M00/00/05/CpoxxF0ZmHuAPlXvAAFe-H5_-Nw961.png', '#ff9801', 'cake-1006', '', 1, 3, 1, '2019-08-26 20:33:06', '2019-08-26 20:33:09');
-INSERT INTO `carousel` VALUES ('c-10021', 'http://122.152.205.72:88/group1/M00/00/05/CpoxxF0ZmH6AeuRrAAEZviPhyQ0768.png', '#55be59', '', '133', 2, 4, 1, '2019-08-28 20:33:06', '2019-08-28 20:33:09');
+INSERT INTO `carousel` VALUES ('c-10011', 'http://127.0.0.1:8082/CpoxxF0ZmG-ALsPRAAEX2Gk9FUg848.png', '#f44661', 'nut-1004', '', 1, 1, 1, '2019-08-27 20:33:06', '2019-08-24 20:33:09');
+INSERT INTO `carousel` VALUES ('c-10013', 'http://127.0.0.1:8082/CpoxxF0ZmHiAWwR7AAFdqZHw8jU876.png', '#000240', '', '51', 2, 2, 1, '2019-08-25 20:33:06', '2019-08-25 20:33:09');
+INSERT INTO `carousel` VALUES ('c-10015', 'http://127.0.0.1:8082/CpoxxF0ZmHuAPlXvAAFe-H5_-Nw961.png', '#ff9801', 'cake-1006', '', 1, 3, 1, '2019-08-26 20:33:06', '2019-08-26 20:33:09');
+INSERT INTO `carousel` VALUES ('c-10021', 'http://127.0.0.1:8082/CpoxxF0ZmH6AeuRrAAEZviPhyQ0768.png', '#55be59', '', '133', 2, 4, 1, '2019-08-28 20:33:06', '2019-08-28 20:33:09');
 COMMIT;
 
 -- ----------------------------
