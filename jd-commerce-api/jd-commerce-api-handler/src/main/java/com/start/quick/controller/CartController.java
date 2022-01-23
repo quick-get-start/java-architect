a5f253773678c8cb9881f9256ca22dda7c47f951
张喜硕 <zhangxishuo1998@outlook.com>
2022-01-23 10:54:00 +0800

    购物车删除商品

--- jd-commerce-api/jd-commerce-api-handler/src/main/java/com/start/quick/controller/CartController.java
+++ jd-commerce-api/jd-commerce-api-handler/src/main/java/com/start/quick/controller/CartController.java
@@ -26,4 +26,22 @@ public class CartController {
 
         return JSONResult.ok("添加到购物车成功");
     }
+
+    @PostMapping("del")
+    public JSONResult<Void> add(@RequestParam String userId,
+                                @RequestParam String specId,
+                                HttpServletRequest request,
+                                HttpServletResponse response) {
+        if (StringUtils.isBlank(userId)) {
+            return JSONResult.build(CartResultCode.INVALID_REQUEST_PARAM, "用户不存在");
+        }
+
+        if (StringUtils.isBlank(specId)) {
+            return JSONResult.build(CartResultCode.INVALID_REQUEST_PARAM, "商品不存在");
+        }
+
+        // todo: 用户在页面删除购物车中的数据，如果用户在登录的情况下，需要同步删除购物车到redis缓存
+
+        return JSONResult.ok("购物车删除商品成功");
+    }
 }
