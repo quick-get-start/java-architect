436351fc3940438cc7ab579fe25034988ecdddef
张喜硕 <zhangxishuo1998@outlook.com>
2022-01-24 16:56:34 +0800

    下单

---
+++ jd-commerce-api/jd-commerce-api-handler/src/main/java/com/start/quick/code/OrderResultCode.java
@@ -0,0 +1,11 @@
+package com.start.quick.code;
+
+import com.start.quick.common.ResultCode;
+
+public class OrderResultCode extends ResultCode {
+
+    /**
+     * 下单异常
+     */
+    public static final Integer ORDER_ERROR = 1;
+}
--- jd-commerce-api/jd-commerce-api-handler/src/main/java/com/start/quick/controller/OrdersController.java
+++ jd-commerce-api/jd-commerce-api-handler/src/main/java/com/start/quick/controller/OrdersController.java
@@ -1,7 +1,9 @@
 package com.start.quick.controller;
 
+import com.start.quick.code.OrderResultCode;
 import com.start.quick.common.JSONResult;
 import com.start.quick.model.OrderSubmitModel;
+import com.start.quick.service.OrderService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("orders")
 public class OrdersController {
 
+    private final OrderService orderService;
+
+    public OrdersController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
     @PostMapping("create")
     public JSONResult<Void> create(@RequestBody OrderSubmitModel submitModel) {
+        try {
+            this.orderService.create(submitModel);
+        } catch (RuntimeException exception) {
+            return JSONResult.build(OrderResultCode.ORDER_ERROR, exception.getMessage());
+        }
+
         return JSONResult.ok("创建订单成功");
     }
 }
