9d296586916a6c7f4242102daef673fe7699fec9
张喜硕 <zhangxishuo1998@outlook.com>
2022-01-28 20:54:39 +0800

    upload-avatar

---
+++ jd-commerce-api/jd-commerce-api-handler/src/main/java/com/start/quick/code/CenterUserResultCode.java
@@ -0,0 +1,6 @@
+package com.start.quick.code;
+
+import com.start.quick.common.ResultCode;
+
+public class CenterUserResultCode extends ResultCode {
+}
--- jd-commerce-api/jd-commerce-api-handler/src/main/java/com/start/quick/controller/CenterUserController.java
+++ jd-commerce-api/jd-commerce-api-handler/src/main/java/com/start/quick/controller/CenterUserController.java
@@ -1,15 +1,21 @@
 package com.start.quick.controller;
 
+import com.start.quick.code.CenterUserResultCode;
 import com.start.quick.common.JSONResult;
 import com.start.quick.entity.Users;
 import com.start.quick.http.UserCommonResponse;
 import com.start.quick.service.CenterUserService;
 import com.start.quick.utils.CookieUtils;
 import com.start.quick.utils.JsonUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 @RestController
 @RequestMapping("userInfo")
@@ -17,8 +23,41 @@ public class CenterUserController {
 
     private final CenterUserService centerUserService;
 
+    private static final String IMAGE_USER_FACE_LOCATION = "./images/avatar";
+
     public CenterUserController(CenterUserService centerUserService) {
         this.centerUserService = centerUserService;
+    }
+
+    @PostMapping("updateAvatar")
+    public JSONResult<Void> updateAvatar(@RequestParam String userId,
+                                         MultipartFile file) {
+        if (ObjectUtils.isEmpty(file)) {
+            return JSONResult.build(CenterUserResultCode.INVALID_REQUEST_PARAM, "文件不能为空!");
+        }
+
+        String fileName = file.getOriginalFilename();
+        if (StringUtils.isNotBlank(fileName)) {
+            String[] arr = fileName.split("\\.");
+            String suffix = arr[arr.length - 1];
+            String name = "face-" + userId + "." + suffix;
+
+            String filePath = IMAGE_USER_FACE_LOCATION + File.separator + userId + File.separator + name;
+            File avatar = new File(filePath);
+            if (avatar.getParentFile() != null) {
+                //noinspection ResultOfMethodCallIgnored
+                avatar.getParentFile().mkdirs();
+            }
+
+            try (FileOutputStream outputStream = new FileOutputStream(avatar)) {
+                InputStream inputStream = file.getInputStream();
+                IOUtils.copy(inputStream, outputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return JSONResult.ok("头像更新成功");
     }
 
     @PostMapping("update")
